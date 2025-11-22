db4494352c4a51712e414cd7cf103cc35b33955d
ferideberisha <93921511+ferideberisha@users.noreply.github.com>
2025-11-22 19:40:00 +0100

    Extract full text from Boing Boing articles

--- scripts/extract_news.py
+++ scripts/extract_news.py
@@ -611,3 +611,15 @@ class Preprocessing:
 
         except:
             return "None"
+
+    def get_boing_boing(self, url):
+        try:
+            news_response = requests.get(url)
+            soup = BeautifulSoup(news_response.content, features="html.parser")
+            content_div = soup.find('section', class_="entry-content")
+            paragraphs = content_div.find_all('p')
+            text_content = ' '.join([paragraph.get_text(strip=True) for paragraph in paragraphs])
+            return text_content
+
+        except:
+            return "None"
