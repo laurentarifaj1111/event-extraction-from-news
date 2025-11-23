1918e8219e62b88936267dca93c96e771862d9ae
ferideberisha <93921511+ferideberisha@users.noreply.github.com>
2025-11-23 14:46:41 +0100

    Add scraper for Punch post-content articles

--- scripts/extract_news.py
+++ scripts/extract_news.py
@@ -636,3 +636,15 @@ class Preprocessing:
 
         except:
             return "None"
+
+    def get_punch(self, url):
+        try:
+            news_response = requests.get(url)
+            soup = BeautifulSoup(news_response.content, features="html.parser")
+            content_div = soup.find('div', class_="post-content")
+            paragraphs = content_div.find_all('p')
+            text_content = ' '.join([paragraph.get_text(strip=True) for paragraph in paragraphs])
+            return text_content
+
+        except:
+            return "None"
