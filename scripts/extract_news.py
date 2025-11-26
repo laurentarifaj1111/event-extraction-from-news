4dc66e6bdfcd8d0fdeb3b4b7be8e74c93d84d163
ferideberisha <93921511+ferideberisha@users.noreply.github.com>
2025-11-26 19:15:39 +0100

    Add ReadWrite scraper and extract articles

--- scripts/extract_news.py
+++ scripts/extract_news.py
@@ -673,3 +673,15 @@ class Preprocessing:
 
         except:
             return "None"
+
+    def get_readwrite(self, url):
+        try:
+            news_response = requests.get(url)
+            soup = BeautifulSoup(news_response.content, features="html.parser")
+            content_div = soup.find('div', class_="entry-content col-md-10")
+            paragraphs = content_div.find_all('p')
+            text_content = ' '.join([paragraph.get_text(strip=True) for paragraph in paragraphs[:-1]])
+            return text_content
+
+        except:
+            return "None"
