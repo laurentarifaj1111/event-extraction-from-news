5e045fcecf0edd7f3817c32b9b5d913d47517e05
ferideberisha <93921511+ferideberisha@users.noreply.github.com>
2025-12-01 19:37:34 +0100

    Add Indian Express scraper

--- scripts/extract_news.py
+++ scripts/extract_news.py
@@ -729,3 +729,15 @@ class Preprocessing:
             return text_content
         except:
             return "None"
+
+    def get_indian_express(self, url):
+        try:
+            news_response = requests.get(url)
+            soup = BeautifulSoup(news_response.content, features="html.parser")
+            content_div = soup.find('div', class_="story_details")
+            paragraphs = content_div.find_all('p')
+            text_content = ' '.join([paragraph.get_text(strip=True) for paragraph in paragraphs])
+            return text_content
+
+        except:
+            return "None"
